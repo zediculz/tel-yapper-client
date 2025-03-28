6d3b6b623128adbf878606f6329f09facdf0455e
[zedic] <oluwaseyifunmitan1@gmail.com>
2025-03-28 18:58:12 +0100

    client work

--- dist/tel-yapper.js
+++ dist/tel-yapper.js
@@ -1,10 +1,51 @@
-function c(n) {
-  let t = 0;
-  const e = (o) => {
-    t = o, n.innerHTML = `count is ${t}`;
-  };
-  n.addEventListener("click", () => e(++t)), e(0);
+const c = "https://tel-yapper-81.deno.dev", d = {
+  Accept: "*/*",
+  "User-Agent": "Tel Yapper Client",
+  "Content-Type": "application/x-www-form-urlencoded"
+};
+async function i(s, e) {
+  const t = `type=${s}&chat=${e}`;
+  return await (await fetch(`${c}/check`, {
+    method: "POST",
+    body: t,
+    headers: d
+  })).json();
+}
+async function p(s, e, t) {
+  let n = `mode=${e}&id=${s}&message=${t}`;
+  return await (await fetch(`${c}/bot`, {
+    method: "POST",
+    body: n,
+    headers: d
+  })).json();
+}
+async function h(s, e, t, n) {
+  let a = `type=${s}&chat=${e}&mode=${t}&message=${n}`;
+  return await (await fetch(`${c}/botd`, {
+    method: "POST",
+    body: a,
+    headers: d
+  })).json();
+}
+class y {
+  /**@param type the type of chat your choose, eg channel or group. @param chat the name of your group or channel. */
+  async check(e, t) {
+    return await i(e, t);
+  }
+  //check, check bots id @done
+  //log, send message
+  //direct, send message with other option
+  /** @param message the message your want send. @param id your chat id. @param mode the mode your want log eg basic, order, error, info and warn, empty mode is the same as basic. */
+  async send(e, t) {
+    const { id: n, mode: a } = t;
+    return await p(n, a === void 0 || a === "" ? "basic" : a, e);
+  }
+  /** @param message the message your want send. @param id your chat id. @param mode the mode your want log eg basic, order, error, info and warn, empty mode is the same as basic. */
+  async direct(e, t) {
+    const { mode: n, type: a, chat: o } = t;
+    return await h(a, o, n === void 0 || n === "" ? "basic" : n, e);
+  }
 }
 export {
-  c as setupCounter
+  y as default
 };
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
-import { setupCounter } from './lib/main.js'
+import TeleYapper  from './lib/main.js'
 
 
+const tele = new TeleYapper()
+
+//let res = await tele.check("channel", "Logyapper ch")
+
+const s = await tele.send('message example', {
+    id: 12345,
+    mode: "order"
+})
+
+//console.log(s)
+
+
+const ss = await tele.direct("direct msg example", {
+    type: "channel",
+    chat: "Logyapperch"
+})
+
+console.log(ss)
+console.log(tele)
