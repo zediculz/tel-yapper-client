0e9e0627e6a56b5ee60c3afb1cbf04fb4c1e6f4b
[zedic] <oluwaseyifunmitan1@gmail.com>
2025-03-28 17:09:09 +0100

    some changes

---
+++ dist/tel-yapper.js
@@ -0,0 +1,10 @@
+function c(n) {
+  let t = 0;
+  const e = (o) => {
+    t = o, n.innerHTML = `count is ${t}`;
+  };
+  n.addEventListener("click", () => e(++t)), e(0);
+}
+export {
+  c as setupCounter
+};
